caa163985ea19d8704d7c392e6716eff3e0e0161
lucca.ssouza <lucca.ssouza@TAU0588413W10-1>
2024-08-30 08:36:26 -0300

    Painel estoque

---
+++ projetoMonkeyShop/src/view/FrmEstoque.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace projetoMonkeyShop.src.view
+{
+    partial class FrmEstoque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmEstoque
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 655);
+            this.Name = "FrmEstoque";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmEstoque";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
