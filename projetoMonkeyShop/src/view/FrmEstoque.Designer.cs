9acb6a5712aeeae8a3e46a6657a5c487165519e1
lucca.ssouza <lucca.ssouza@TAU0588413W10-1>
2024-08-30 09:47:25 -0300

    tabela na tela de estoque

--- projetoMonkeyShop/src/view/FrmEstoque.Designer.cs
+++ projetoMonkeyShop/src/view/FrmEstoque.Designer.cs
@@ -31,12 +31,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -51,12 +47,27 @@
             this.textBox11 = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
             this.btnBuscarProd = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btnCancelarProd = new System.Windows.Forms.Button();
             this.btnAlterarProd = new System.Windows.Forms.Button();
             this.btnSalvarProd = new System.Windows.Forms.Button();
             this.btnCriarProd = new System.Windows.Forms.Button();
             this.btnExcluirProd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoriaProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tamanhoProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.corProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qtdProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precoProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -83,30 +94,6 @@
             this.textBox3.Size = new System.Drawing.Size(212, 38);
             this.textBox3.TabIndex = 0;
             // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(245, 113);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(212, 38);
-            this.textBox4.TabIndex = 0;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(884, 113);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(92, 38);
-            this.textBox5.TabIndex = 0;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(719, 113);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(139, 38);
-            this.textBox6.TabIndex = 0;
-            // 
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -122,14 +109,6 @@
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(134, 38);
             this.textBox8.TabIndex = 0;
-            // 
-            // textBox9
-            // 
-            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox9.Location = new System.Drawing.Point(172, 196);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(212, 38);
-            this.textBox9.TabIndex = 0;
             // 
             // textBox10
             // 
@@ -266,36 +245,11 @@
             this.btnBuscarProd.Text = "Buscar";
             this.btnBuscarProd.UseVisualStyleBackColor = true;
             // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 10;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 166F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 195F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 146F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 74F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 362);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1028, 295);
-            this.tableLayoutPanel1.TabIndex = 3;
-            // 
             // btnCancelarProd
             // 
             this.btnCancelarProd.Location = new System.Drawing.Point(412, 673);
             this.btnCancelarProd.Name = "btnCancelarProd";
-            this.btnCancelarProd.Size = new System.Drawing.Size(123, 56);
+            this.btnCancelarProd.Size = new System.Drawing.Size(123, 45);
             this.btnCancelarProd.TabIndex = 4;
             this.btnCancelarProd.Text = "Cancelar";
             this.btnCancelarProd.UseVisualStyleBackColor = true;
@@ -304,7 +258,7 @@
             // 
             this.btnAlterarProd.Location = new System.Drawing.Point(571, 673);
             this.btnAlterarProd.Name = "btnAlterarProd";
-            this.btnAlterarProd.Size = new System.Drawing.Size(123, 56);
+            this.btnAlterarProd.Size = new System.Drawing.Size(123, 45);
             this.btnAlterarProd.TabIndex = 4;
             this.btnAlterarProd.Text = "Alterar";
             this.btnAlterarProd.UseVisualStyleBackColor = true;
@@ -313,7 +267,7 @@
             // 
             this.btnSalvarProd.Location = new System.Drawing.Point(735, 673);
             this.btnSalvarProd.Name = "btnSalvarProd";
-            this.btnSalvarProd.Size = new System.Drawing.Size(123, 56);
+            this.btnSalvarProd.Size = new System.Drawing.Size(123, 45);
             this.btnSalvarProd.TabIndex = 4;
             this.btnSalvarProd.Text = "Salvar";
             this.btnSalvarProd.UseVisualStyleBackColor = true;
@@ -322,7 +276,7 @@
             // 
             this.btnCriarProd.Location = new System.Drawing.Point(900, 673);
             this.btnCriarProd.Name = "btnCriarProd";
-            this.btnCriarProd.Size = new System.Drawing.Size(123, 56);
+            this.btnCriarProd.Size = new System.Drawing.Size(123, 45);
             this.btnCriarProd.TabIndex = 4;
             this.btnCriarProd.Text = "Novo";
             this.btnCriarProd.UseVisualStyleBackColor = true;
@@ -331,22 +285,131 @@
             // 
             this.btnExcluirProd.Location = new System.Drawing.Point(261, 673);
             this.btnExcluirProd.Name = "btnExcluirProd";
-            this.btnExcluirProd.Size = new System.Drawing.Size(123, 56);
+            this.btnExcluirProd.Size = new System.Drawing.Size(123, 45);
             this.btnExcluirProd.TabIndex = 4;
             this.btnExcluirProd.Text = "Excluir";
             this.btnExcluirProd.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idProd,
+            this.codProd,
+            this.nomeProd,
+            this.categoriaProd,
+            this.modelProd,
+            this.tamanhoProd,
+            this.corProd,
+            this.qtdProd,
+            this.statusProd,
+            this.precoProd});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 372);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1011, 279);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // idProd
+            // 
+            this.idProd.HeaderText = "ID";
+            this.idProd.Name = "idProd";
+            // 
+            // codProd
+            // 
+            this.codProd.HeaderText = "Cod/Barras";
+            this.codProd.Name = "codProd";
+            // 
+            // nomeProd
+            // 
+            this.nomeProd.HeaderText = "Column1";
+            this.nomeProd.Name = "nomeProd";
+            // 
+            // categoriaProd
+            // 
+            this.categoriaProd.HeaderText = "Categoria";
+            this.categoriaProd.Name = "categoriaProd";
+            // 
+            // modelProd
+            // 
+            this.modelProd.HeaderText = "Modelo";
+            this.modelProd.Name = "modelProd";
+            // 
+            // tamanhoProd
+            // 
+            this.tamanhoProd.HeaderText = "Tamanho";
+            this.tamanhoProd.Name = "tamanhoProd";
+            // 
+            // corProd
+            // 
+            this.corProd.HeaderText = "Cor";
+            this.corProd.Name = "corProd";
+            // 
+            // qtdProd
+            // 
+            this.qtdProd.HeaderText = "Quantidade";
+            this.qtdProd.Name = "qtdProd";
+            // 
+            // statusProd
+            // 
+            this.statusProd.HeaderText = "Status";
+            this.statusProd.Name = "statusProd";
+            // 
+            // precoProd
+            // 
+            this.precoProd.HeaderText = "Preço";
+            this.precoProd.Name = "precoProd";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(719, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 39);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(884, 113);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(92, 39);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(172, 196);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(212, 39);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(245, 112);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(208, 39);
+            this.comboBox4.TabIndex = 6;
             // 
             // FrmEstoque
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1052, 741);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnCriarProd);
             this.Controls.Add(this.btnSalvarProd);
             this.Controls.Add(this.btnAlterarProd);
             this.Controls.Add(this.btnExcluirProd);
             this.Controls.Add(this.btnCancelarProd);
-            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.btnBuscarProd);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -363,16 +426,13 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox10);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox1);
             this.Name = "FrmEstoque";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmEstoque";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -383,12 +443,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -403,11 +459,25 @@
         private System.Windows.Forms.TextBox textBox11;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button btnBuscarProd;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button btnCancelarProd;
         private System.Windows.Forms.Button btnAlterarProd;
         private System.Windows.Forms.Button btnSalvarProd;
         private System.Windows.Forms.Button btnCriarProd;
         private System.Windows.Forms.Button btnExcluirProd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoriaProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tamanhoProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn corProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qtdProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn statusProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precoProd;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
     }
 }
