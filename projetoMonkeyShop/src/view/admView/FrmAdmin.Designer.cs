01ff81bd8e0ac9cbac89c7161f4fc432b3fd0692
Lucca Samuel <samuellucca1002@gmail.com>
2024-09-18 14:30:37 -0300

    inicio da area administrativa

---
+++ projetoMonkeyShop/src/view/admView/FrmAdmin.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace projetoMonkeyShop.src.view.admView
+{
+    partial class FrmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnFornecedores = new System.Windows.Forms.Button();
+            this.btnFuncionarios = new System.Windows.Forms.Button();
+            this.btnRelatorios = new System.Windows.Forms.Button();
+            this.btnEstoque = new System.Windows.Forms.Button();
+            this.pnlMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.Gray;
+            this.pnlMenu.Controls.Add(this.btnEstoque);
+            this.pnlMenu.Controls.Add(this.btnRelatorios);
+            this.pnlMenu.Controls.Add(this.btnFuncionarios);
+            this.pnlMenu.Controls.Add(this.btnFornecedores);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(208, 741);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // btnFornecedores
+            // 
+            this.btnFornecedores.BackColor = System.Drawing.Color.Black;
+            this.btnFornecedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFornecedores.ForeColor = System.Drawing.Color.White;
+            this.btnFornecedores.Location = new System.Drawing.Point(0, 148);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Size = new System.Drawing.Size(208, 65);
+            this.btnFornecedores.TabIndex = 0;
+            this.btnFornecedores.Text = "Fornecedores";
+            this.btnFornecedores.UseVisualStyleBackColor = false;
+            // 
+            // btnFuncionarios
+            // 
+            this.btnFuncionarios.BackColor = System.Drawing.Color.Black;
+            this.btnFuncionarios.ForeColor = System.Drawing.Color.White;
+            this.btnFuncionarios.Location = new System.Drawing.Point(0, 219);
+            this.btnFuncionarios.Name = "btnFuncionarios";
+            this.btnFuncionarios.Size = new System.Drawing.Size(208, 65);
+            this.btnFuncionarios.TabIndex = 0;
+            this.btnFuncionarios.Text = "Funcionários";
+            this.btnFuncionarios.UseVisualStyleBackColor = false;
+            // 
+            // btnRelatorios
+            // 
+            this.btnRelatorios.BackColor = System.Drawing.Color.Black;
+            this.btnRelatorios.ForeColor = System.Drawing.Color.White;
+            this.btnRelatorios.Location = new System.Drawing.Point(0, 290);
+            this.btnRelatorios.Name = "btnRelatorios";
+            this.btnRelatorios.Size = new System.Drawing.Size(208, 65);
+            this.btnRelatorios.TabIndex = 0;
+            this.btnRelatorios.Text = "Relatórios";
+            this.btnRelatorios.UseVisualStyleBackColor = false;
+            // 
+            // btnEstoque
+            // 
+            this.btnEstoque.BackColor = System.Drawing.Color.Black;
+            this.btnEstoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnEstoque.ForeColor = System.Drawing.Color.White;
+            this.btnEstoque.Location = new System.Drawing.Point(0, 361);
+            this.btnEstoque.Name = "btnEstoque";
+            this.btnEstoque.Size = new System.Drawing.Size(208, 65);
+            this.btnEstoque.TabIndex = 0;
+            this.btnEstoque.Text = "Estoque";
+            this.btnEstoque.UseVisualStyleBackColor = false;
+            // 
+            // FrmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 741);
+            this.Controls.Add(this.pnlMenu);
+            this.Name = "FrmAdmin";
+            this.Text = "FrmAdmin";
+            this.pnlMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnFornecedores;
+        private System.Windows.Forms.Button btnEstoque;
+        private System.Windows.Forms.Button btnRelatorios;
+        private System.Windows.Forms.Button btnFuncionarios;
+    }
+}
